51b67111db880fe48d1de72a063b6418594a6e52
betussozgen <fatmabetulozgen@hotmail.com>
2023-01-04 17:57:56 +0300

    first commit

---
+++ console.js
@@ -0,0 +1,9 @@
+/*
+1-Form seçimi
+2-Input Bilgisini UL içerisine ekle
+3-Form içindeki bilgiyi sıfırla
+4-Eğer forma bilgi girilmez ise kullanıcıyı uyar
+5-Passwor'ler aynı değilse uyarı versin.(uyarıyı yana versin)
+
+
+*/
